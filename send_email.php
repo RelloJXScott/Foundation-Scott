73173a9bbe4cabdae769470daf8038395192a321
Tyrell Scott <rellojxscott@gmail.com>
2024-03-06 11:43:41 -0800

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = $_POST["email"];
     $subject = "Foundation Scott Verification Code";
     $message = "Hello " . $_POST["username"] . ",\n\nThank you for signing up with Foundation Scott! Your verification code is: " . $_POST["verificationCode"];
-    $headers = "From: rellojxscott@gmail.com"; // Replace with your email
+    $headers = "From: foundationscott.vercel.app@gmail.com";
 
     mail($to, $subject, $message, $headers);
 }
